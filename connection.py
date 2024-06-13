81d478ecbc8f661eab2a07c9f0c04149b5426489
AbdullahMart <162364327+AbdullahMart@users.noreply.github.com>
2024-06-13 13:43:47 +0200

    updated

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 # Veritabanı bağlantı bilgileri
-database_name = "Employees"
+database_name = "CRM"
 user = "postgres"
 password = "2713"
 host = "localhost"  # Genellikle localhost veya 127.0.0.1
@@ -19,48 +19,25 @@ try:
     # Bağlantı üzerinden bir işlem (cursor) oluşturun
     cursor = connection.cursor()
     
-    # Tablo oluşturma sorgusu
-    create_table_query = """
-    CREATE TABLE IF NOT EXISTS Employees_table(
-        emp_id INT PRIMARY KEY,
-        first_name VARCHAR(15),
-        last_name VARCHAR(15),
-        salary INT,
-        job_title VARCHAR(20),
-        gender VARCHAR(6),
-        hire_date DATE
-    )
+    # kursiyerler ve basvurular tablolarını JOIN ile birleştiren sorgu
+    join_query = """
+    SELECT k.AdSoyad, k.MailAdresi, k.TelefonNumarası, k.PostaKodu, b.ZamanDamgası, b.SuAnkiDurum
+    FROM kursiyerler k
+    INNER JOIN basvurular b ON k.KursiyerID = b.KursiyerID
     """
+    cursor.execute(join_query)
     
-    # Sorguyu çalıştırma
-    cursor.execute(create_table_query)
-    connection.commit()  # Değişiklikleri kaydet
-
-    print("Tablo oluşturuldu veya zaten mevcut.")
-
-    insert_table_query = """
-    INSERT INTO Employees_table(
-        emp_id ,
-        first_name ,
-        last_name ,
-        salary ,
-        job_title ,
-        gender ,
-        hire_date 
-    ) VALUES
-    (17679,'Robert','Gilmore',110000,'Operation Director','Male','2018-09-04'),
-    (26650,'Elvis','Ritter',86000,'Sales Manager','Male','2017-11-24')
-
-    """
-    cursor.execute(insert_table_query)
-    connection.commit()  # Değişiklikleri kaydet
-    print("veriler eklendi")
-
+    # Sonuçları alın
+    combined_results = cursor.fetchall()
+    
+    # Sonuçları yazdırın
+    for row in combined_results:
+        print(row)
 
 except Exception as e:
     print("Bağlantı hatası:", e)
 finally:
-    # Bağlantıyı kapatın"""  """
+    # Bağlantıyı kapatın
     if cursor:
         cursor.close()
     if connection:
